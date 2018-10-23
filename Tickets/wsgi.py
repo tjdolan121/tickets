0a0c54e64bb4bc2488b4d80edeabc73d0145b851
tjdolan121 <tdolanindiana@yahoo.com>
2018-10-23 03:48:00 +0300

    Finalized configuration for Heroku deployment

--- Tickets/wsgi.py
+++ Tickets/wsgi.py
@@ -14,3 +14,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tickets.settings')
 
 application = get_wsgi_application()
+
+# Use whitenoise package to serve static files on Heroku
+
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangoWhiteNoise(application)
+
